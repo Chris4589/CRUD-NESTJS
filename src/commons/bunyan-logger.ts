51f6603dbe526fafb3a3a516ce0c70f290cc5bad
cherrera45 <cherrera@is4tech.com>
2023-03-05 18:22:57 -0600

    first commit

--- src/commons/bunyan-logger.ts
+++ src/commons/bunyan-logger.ts
@@ -1,1 +1,42 @@
-import Logger, { createLogger, stdSerializers, LoggerOptions } from 'bunyan';import * as apiInfo from '../../package.json';import * as os from 'os';import * as ip from 'ip';import * as path from 'path';import { Injectable, Logger as Log4 } from '@nestjs/common';@Injectable()export class BunyanLogger {  write(): Logger {    const options: LoggerOptions = {      // this funcion needs "resolveJsonModule": true in tsconfig.json      name: apiInfo.name,      hostname: `${os.hostname()}:${ip.address()}`,      streams: [{        level: 'debug',        name: 'debug',        path: process.env.SERVER_LOG_DEBUG,      }, {        level: 'error',        name: 'error',        path: process.env.SERVER_LOG_ERROR,      }],      responseCode: '',    };    return createLogger(options);  }  logError(responseCode: number, className: string, message: string): void {    const printer: Log4 = new Log4(className);    printer.error(message);    const logger = this.write();    logger.error({ responseCode }, message);  }  logInfo(responseCode: number, className: string, message: string): void {    const printer: Log4 = new Log4(className);    printer.log(message);    const logger = this.write();    logger.info({ responseCode }, message);  }}
+import Logger, { createLogger, stdSerializers, LoggerOptions } from 'bunyan';
+import * as apiInfo from '../../package.json';
+import * as os from 'os';
+import * as ip from 'ip';
+import * as path from 'path';
+import { Injectable, Logger as Log4 } from '@nestjs/common';
+
+@Injectable()
+export class BunyanLogger {
+  write(): Logger {
+    const options: LoggerOptions = {
+      // this funcion needs "resolveJsonModule": true in tsconfig.json
+      name: apiInfo.name,
+      hostname: `${os.hostname()}:${ip.address()}`,
+      streams: [{
+        level: 'debug',
+        name: 'debug',
+        path: process.env.SERVER_LOG_DEBUG,
+      }, {
+        level: 'error',
+        name: 'error',
+        path: process.env.SERVER_LOG_ERROR,
+      }],
+      responseCode: '',
+    };
+    return createLogger(options);
+  }
+
+  logError(responseCode: number, className: string, message: string): void {
+    const printer: Log4 = new Log4(className);
+    printer.error(message);
+    const logger = this.write();
+    logger.error({ responseCode }, message);
+  }
+
+  logInfo(responseCode: number, className: string, message: string): void {
+    const printer: Log4 = new Log4(className);
+    printer.log(message);
+    const logger = this.write();
+    logger.info({ responseCode }, message);
+  }
+}
